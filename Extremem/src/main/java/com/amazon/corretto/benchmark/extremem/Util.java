7164e955b8123021847352884b290e48b0b989f3
Kelvin Nilsen <kdnilsen@amazon.com>
2020-08-06 19:44:40 +0000

    Isolate code with 3rd party copyright

--- Extremem/src/main/java/com/amazon/corretto/benchmark/extremem/Util.java
+++ Extremem/src/main/java/com/amazon/corretto/benchmark/extremem/Util.java
@@ -7,7 +7,7 @@ import java.util.Collection;
 
 class Util {
 
-  static boolean Is64Bit = is64bit0();
+  static boolean Is64Bit = is64bit();
 
   /* Every String object (even substrings) is presumed to consist of
    * the following information:
@@ -56,35 +56,8 @@ class Util {
     System.exit(-1);
   }
 
-  /**
-   * This comment describes the implementation of is64bit0. This code is:
-   *   
-   * Copyright 2016 higherfrequencytrading.com
-   *
-   * Licensed under the Apache License, Version 2.0 (the "License");
-   * you may not use this file except in compliance with the License.
-   * You may obtain a copy of the License at
-   *
-   *     http://www.apache.org/licenses/LICENSE-2.0
-   *
-   * Unless required by applicable law or agreed to in writing, software
-   * distributed under the License is distributed on an "AS IS" BASIS,
-   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-   * implied.
-   * See the License for the specific language governing permissions and
-   * limitations under the License.
-   */
-  private static boolean is64bit0() {
-    String systemProp = System.getProperty("com.ibm.vm.bitmode");
-    if (systemProp != null) {
-      return "64".equals(systemProp);
-    }
-    systemProp = System.getProperty("sun.arch.data.model");
-    if (systemProp != null) {
-      return "64".equals(systemProp);
-    }
-    systemProp = System.getProperty("java.vm.version");
-    return systemProp != null && systemProp.contains("_64");
+  static boolean is64Bit() {
+    return Is64Bit.is64bit0();
   }
 
   /**

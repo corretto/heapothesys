980d95c44beed36aec21d5a2523a257a3685ebdb
William Kemper <kemperw@amazon.com>
2024-12-05 11:38:27 -0800

    Remove some System.exit usages

--- HyperAlloc/src/test/java/com/amazon/corretto/benchmark/hyperalloc/HyperAllocTest.java
+++ HyperAlloc/src/test/java/com/amazon/corretto/benchmark/hyperalloc/HyperAllocTest.java
@@ -4,26 +4,9 @@ package com.amazon.corretto.benchmark.hyperalloc;
 
 import org.junit.jupiter.api.Test;
 
-import static com.github.stefanbirkner.systemlambda.SystemLambda.catchSystemExit;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-
 class HyperAllocTest {
-
-    @Test
-    void SimpleRunTest() {
-        HyperAlloc.main(new String[]{"-u", "simple", "-d", "5"});
-    }
-
     @Test
     void DefaultRunTypeTest() {
         HyperAlloc.main(new String[]{"-d", "5"});
-    }
-
-    @Test
-    void UnknownRunTypeTest() throws Exception {
-        int status = catchSystemExit(
-                () -> HyperAlloc.main(new String[]{"-u", "unknown", "-a", "5"}));
-        assertThat(status, is(1));
     }
 }

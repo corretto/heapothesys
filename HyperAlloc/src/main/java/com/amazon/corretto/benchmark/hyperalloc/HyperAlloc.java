4c7e5569e58da68318038541cd03b11d02ddbf83
William Kemper <71722661+earthling-amzn@users.noreply.github.com>
2024-12-06 13:36:02 -0800

    Remove some System.exit usages (#78)
    After JEP-486, it is no longer possible to intercept System.exit calls (used only for unit tests).

--- HyperAlloc/src/main/java/com/amazon/corretto/benchmark/hyperalloc/HyperAlloc.java
+++ HyperAlloc/src/main/java/com/amazon/corretto/benchmark/hyperalloc/HyperAlloc.java
@@ -3,29 +3,15 @@
 package com.amazon.corretto.benchmark.hyperalloc;
 
 public final class HyperAlloc {
-    private static final String DEFAULT_RUN_TYPE = "simple";
-
     private HyperAlloc() {}
 
     public static void main(String[] args) {
-
-        switch (findRunType(args)) {
-            case "simple" :
-                new SimpleRunner(new SimpleRunConfig(args)).start();
-                break;
-            default:
-                System.out.println("Current supported run type (-u): simple.");
-                System.exit(1);
+        try {
+            new SimpleRunner(new SimpleRunConfig(args)).start();
+        } catch (IllegalArgumentException e) {
+            SimpleRunConfig.usage();
+            System.exit(1);
         }
-    }
-
-    private static String findRunType(final String[] args) {
-        for (int i = 0; i < args.length - 1; i++) {
-            if (args[i].equals("-u")) {
-                return args[i + 1];
-            }
-        }
-        return DEFAULT_RUN_TYPE;
     }
 }
 
